710341c7dc5ec587067808be3bdd843889a5270f
Ricardo <ricardojvsilva10@gmail.com>
2018-12-23 01:55:19 +0000

    layout changes

--- app/src/main/java/pt/amov/logic/Statistic.java
+++ app/src/main/java/pt/amov/logic/Statistic.java
@@ -5,7 +5,7 @@ public class Statistic {
 	public int PLAYER;
 	public int AI;
 
-	public Statistic(int PLAYER, int AI) {
+	Statistic(int PLAYER, int AI) {
 		this.PLAYER = PLAYER;
 		this.AI = AI;
 	}
